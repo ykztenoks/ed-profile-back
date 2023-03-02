ca68bf0bf60156f3dc3603525cbb1ce5434d6022
ed-coleman <edcolemancode@gmail.com>
2023-03-02 18:40:28 +0000

    add outstanding CRUD routes to enable create new user, get all users, find, update and delete single user

--- Project-3-backend/models/User.model.js
+++ Project-3-backend/models/User.model.js
@@ -20,7 +20,7 @@ const userSchema = new Schema(
       validate: [validateEmail, 'Please fill a valid email address'],
       match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
     },
-    password: {
+    passwordHash: {
       type: String,
       required: [true, 'Password is required.']
     }
